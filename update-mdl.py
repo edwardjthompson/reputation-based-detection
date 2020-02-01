aa8c0df7d24352ae727db524c8d060d54b89d5a4
edwardjthompson <edward.j.thompson@gmail.com>
2020-02-01 12:16:12 -0800

    moved curl into python and removed last line that caused decode to fail

--- update-mdl.py
+++ update-mdl.py
@@ -1,4 +1,27 @@
 import csv
+import io
+import requests
+
+r = requests.get('https://gitlab.cs.wwu.edu/tsikerm/assignment-files/raw/master/mdl.csv')
+
+# print(r.text)
+
+with open('mdl.csv', 'w') as mdlcsv:
+    mdlcsv.write(r.text)
+    # writer = csv.writer(mdlcsv)
+    # writer.writerow(r.text)
+# print(r.content)
+with open('mdl.csv', 'r+') as f:
+    lines = f.readlines()
+    lines = lines[:-1]
+    f.seek(0)
+    f.truncate()
+    for line in lines:
+        f.write(line)
+    # f.write(lines)
+
+# for line in r.text:
+#     print(line)
 
 with open('mdl.csv', newline='') as csvfile:
     reader = csv.reader(csvfile, delimiter=',', quotechar='"')
