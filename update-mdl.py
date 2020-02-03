6c57fe23fd31bfb946d9067a985c45089ad48035
edwardjthompson <edward.j.thompson@gmail.com>
2020-02-02 17:35:31 -0800

    seeing if import request passes

--- update-mdl.py
+++ update-mdl.py
@@ -1,16 +1,16 @@
 import csv
 import re
 # import io
-# import requests
+import requests
 
-# r = requests.get('https://gitlab.cs.wwu.edu/tsikerm/assignment-files/raw/master/mdl.csv')
+r = requests.get('https://gitlab.cs.wwu.edu/tsikerm/assignment-files/raw/master/mdl.csv')
 
 # print(r.text)
 
-# with open('mdl.csv', 'w') as mdlcsv:
-#     mdlcsv.write(r.text)
-    # writer = csv.writer(mdlcsv)
-    # writer.writerow(r.text)
+with open('mdl.csv', 'w') as mdlcsv:
+    mdlcsv.write(r.text)
+    writer = csv.writer(mdlcsv)
+    writer.writerow(r.text)
 # print(r.content)
 with open('mdl.csv', 'r+') as f:
     lines = f.readlines()
@@ -34,22 +34,23 @@ with open('mdl.csv', newline='') as csvfile:
     with open('mdl.list', 'w') as mdlfile:
         writer = csv.writer(mdlfile)
         for line in reader:
-            # print(line)
+            print(line)
             # print(len(line))
             if len(line) != 0:
-                # print(line)
+                # print(line, len(line))
                 rep = len(line[1])
                 # ip = line[2]
                 ip = re.findall( r'[0-9]+(?:\.[0-9]+){3}', line[2] )
                 # ip = "140.160." + str(counter2) + "." + str(counter)
-                counter += 1
-                if counter > 254:
-                    counter = 1
-                    counter2 += 1
+                # counter += 1
+                # if counter > 254:
+                #     counter = 1
+                #     counter2 += 1
                 # print(line[2],"->",ip)
-                num = ip[0]
+                # num = ip[0]
                 # print(num)
 
+                # print(ip[0])
                 # rep = urlLen // 127
                 if rep < 1:
                     rep = 1
@@ -59,4 +60,4 @@ with open('mdl.csv', newline='') as csvfile:
                     rep = rep // 5
                 if rep < 1:
                     rep = 1
-                writer.writerow((ip[0], 1, rep))
+                # writer.writerow((ip[0], 1, rep))
